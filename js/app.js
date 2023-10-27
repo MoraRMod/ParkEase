f565aca80bbf1a7375b5c9e7694d43c43030b7ae
MoraRMod <ralerwip@gmail.com>
2023-10-27 17:28:13 -0600

    funciones de hover de las imagenes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const links = document.querySelectorAll(".navigation__link");
+const iphoneScanner = document.getElementById("iphoneScanner");
+const fondoScanner = document.getElementById("fondoScanner");
 
 links.forEach((link) => {
 	link.addEventListener("mouseover", () => {
@@ -8,4 +10,18 @@ links.forEach((link) => {
 	link.addEventListener("mouseout", () => {
 		link.style.transform = "scale(1)";
 	});
+});
+
+iphoneScanner.addEventListener("mouseover", () => {
+	iphoneScanner.style.transition = "transform 0.3s";
+	fondoScanner.style.transition = "transform 0.39s";
+	iphoneScanner.style.transform = "scale(1.1)";
+	fondoScanner.style.transform = "scale(1.1)";
+});
+
+iphoneScanner.addEventListener("mouseout", () => {
+	iphoneScanner.style.transition = "transform 0.3s";
+	fondoScanner.style.transition = "transform 0.39s";
+	iphoneScanner.style.transform = "scale(1)";
+	fondoScanner.style.transform = "scale(1)";
 });
