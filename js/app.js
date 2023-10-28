5ef64123be72a9442a0104642f7ea0bf492c8552
MoraRMod <ralerwip@gmail.com>
2023-10-27 23:03:00 -0600

    codigo para el slide

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ const iphoneScanner = document.getElementById("iphoneScanner");
 const fondoScanner = document.getElementById("fondoScanner");
 const blurScanner = document.getElementById("blurScanner");
 
+let slideIndex = 0;
+let timer;
+
 links.forEach((link) => {
 	link.addEventListener("mouseover", () => {
 		link.style.transform = "scale(1.15)";
@@ -30,3 +33,37 @@ iphoneScanner.addEventListener("mouseout", () => {
 	blurScanner.style.transform = "scale(1)";
 	fondoScanner.style.transform = "scale(1)";
 });
+
+function plusSlides(n) {
+	clearTimeout(timer);
+	showSlides((slideIndex += n));
+
+	timer = setTimeout(showSlides, 7000);
+}
+
+function showSlides() {
+	const slides = document.getElementsByClassName("testimonials__user");
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	if (slideIndex >= slides.length) {
+		slideIndex = 0;
+	}
+
+	if (slideIndex < 0) {
+		slideIndex = slides.length - 1;
+	}
+
+	slides[slideIndex].style.display = "block";
+
+	setTimeout(() => {
+		slides[slideIndex].style.opacity = 1;
+	}, 10);
+
+	slideIndex++;
+	timer = setTimeout(showSlides, 5000);
+}
+
+showSlides();
